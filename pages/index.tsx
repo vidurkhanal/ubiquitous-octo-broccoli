a659460c1d649f626dde220f8f312ac20d877614
Vidur Khanal <vidurkhanal@icloud.com>
2021-02-07 16:20:44 -0500

    NAVBAR ADDED

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
-import Head from 'next/head'
-
+import Head from "next/head";
+import { Navbar } from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>Hello World</title>
       </Head>
+      <Navbar />
       Hello World
     </div>
-  )
+  );
 }
