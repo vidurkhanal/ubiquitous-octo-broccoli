bfa02f70e8b2371607a93e43b3958d335d0e1ea0
Vidur Khanal <vidurkhanal@icloud.com>
2021-02-07 16:17:08 -0500

    UTILITY TO CONVERT RESPONSE IN ARRAY TO OBJECT

--- utils/toErrorMap.ts
+++ utils/toErrorMap.ts
@@ -1,11 +1,10 @@
 import { FieldError } from "../generated/graphql";
 
-const toErrorMap = (errors: FieldError[]) => {
+let toErrorMap;
+export default toErrorMap = (errors: FieldError[]) => {
   const errorMap: Record<string, string> = {};
   errors.forEach(({ field, message }) => {
     errorMap[field.toLowerCase()] = message;
   });
   return errorMap;
 };
-
-export default toErrorMap;
