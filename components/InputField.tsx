95e54e29cac5f51e8726e75a71fe86d3d5145584
Vidur Khanal <vkhanal@caldwell.edu>
2021-03-03 16:44:33 -0500

    INPUT FIELD CHANGED TO HANDLE TEXT AREA

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -3,6 +3,7 @@ import {
   FormLabel,
   FormErrorMessage,
   Input,
+  Textarea,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 import { InputHTMLAttributes } from "react";
@@ -13,19 +14,31 @@ type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   type: string;
   isRequired?: boolean;
+  textarea?: boolean;
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  placeholder,
+  type,
+  isRequired,
+  textarea,
+  ...props
+}) => {
   const [field, { error }] = useField(props);
+  let InputorTextArea = Input;
+  if (textarea) {
+    InputorTextArea = Textarea;
+  }
   return (
     <FormControl isInvalid={error ? true : false}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-      <Input
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <InputorTextArea
         {...field}
         id={field.name}
-        placeholder={props.placeholder}
-        type={props.type}
-        required={props.isRequired}
+        placeholder={placeholder}
+        type={type}
+        required={isRequired}
       />
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
