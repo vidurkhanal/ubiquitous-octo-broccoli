ea6d55a7ec4c2370a7a92a230eca2bb4d3da495b
Vidur Khanal <vkhanal@caldwell.edu>
2021-03-07 20:22:01 -0500

    Is Auth Checker created

---
+++ utils/useIsAuth.ts
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useMyselfQuery } from "../generated/graphql";
+
+export const useIsAuth = () => {
+  const router = useRouter();
+  const [{ data, fetching }] = useMyselfQuery();
+  useEffect(() => {
+    if (!fetching && !data?.me) {
+      router.push("/login");
+    }
+  }, [fetching, data, router]);
+};
