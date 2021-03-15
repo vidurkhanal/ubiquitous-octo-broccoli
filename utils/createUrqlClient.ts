433edba77000be0a4b02952f28687fb0787c549a
Vidur Khanal <vkhanal@caldwell.edu>
2021-03-14 22:11:28 -0400

    PAGINATION CACHE ADDED

--- utils/createUrqlClient.ts
+++ utils/createUrqlClient.ts
@@ -1,5 +1,5 @@
 import { dedupExchange, Exchange, fetchExchange } from "urql";
-import { cacheExchange } from "@urql/exchange-graphcache";
+import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
 import {
   LogoutMutation,
   MyselfQuery,
@@ -22,6 +22,109 @@ const errorExchange: Exchange = ({ forward }) => (ops$) => {
   );
 };
 
+export const cursorPagination = (): Resolver => {
+  // const compareArgs = (
+  //   fieldArgs: Variables,
+  //   connectionArgs: Variables
+  // ): boolean => {
+  //   for (const key in connectionArgs) {
+  //     if (key === cursorArgument || key === limitArgument) {
+  //       continue;
+  //     } else if (!(key in fieldArgs)) {
+  //       return false;
+  //     }
+
+  //     const argA = fieldArgs[key];
+  //     const argB = connectionArgs[key];
+
+  //     if (
+  //       typeof argA !== typeof argB || typeof argA !== 'object'
+  //         ? argA !== argB
+  //         : stringifyVariables(argA) !== stringifyVariables(argB)
+  //     ) {
+  //       return false;
+  //     }
+  //   }
+
+  //   for (const key in fieldArgs) {
+  //     if (key === cursorArgument || key === limitArgument) {
+  //       continue;
+  //     }
+  //     if (!(key in connectionArgs)) return false;
+  //   }
+
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+
+    const allFields = cache.inspectFields(entityKey);
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+    const results: string[] = [];
+
+    fieldInfos.forEach((fi) => {
+      const data = cache.resolve(entityKey, fi.fieldKey) as string[];
+      results.push(...data);
+    });
+
+    return results;
+  };
+  //   const visited = new Set();
+  //   let result: NullArray<string> = [];
+  //   let prevOffset: number | null = null;
+
+  //   for (let i = 0; i < size; i++) {
+  //     const { fieldKey, arguments: args } = fieldInfos[i];
+  //     if (args === null || !compareArgs(fieldArgs, args)) {
+  //       continue;
+  //     }
+
+  //     const links = cache.resolve(entityKey, fieldKey) as string[];
+  //     const currentOffset = args[cursorArgument];
+
+  //     if (
+  //       links === null ||
+  //       links.length === 0 ||
+  //       typeof currentOffset !== 'number'
+  //     ) {
+  //       continue;
+  //     }
+
+  //     const tempResult: NullArray<string> = [];
+
+  //     for (let j = 0; j < links.length; j++) {
+  //       const link = links[j];
+  //       if (visited.has(link)) continue;
+  //       tempResult.push(link);
+  //       visited.add(link);
+  //     }
+
+  //     if (
+  //       (!prevOffset || currentOffset > prevOffset) ===
+  //       (mergeMode === 'after')
+  //     ) {
+  //       result = [...result, ...tempResult];
+  //     } else {
+  //       result = [...tempResult, ...result];
+  //     }
+
+  //     prevOffset = currentOffset;
+  //   }
+
+  //   const hasCurrentPage = cache.resolve(entityKey, fieldName, fieldArgs);
+  //   if (hasCurrentPage) {
+  //     return result;
+  //   } else if (!(info as any).store.schema) {
+  //     return undefined;
+  //   } else {
+  //     info.partial = true;
+  //     return result;
+  //   }
+  // };
+};
+
 export const createUrqlClient = (ssrExchange: any) => ({
   url: "http://localhost:8080/graphql",
   fetchOptions: {
@@ -30,6 +133,11 @@ export const createUrqlClient = (ssrExchange: any) => ({
   exchanges: [
     dedupExchange,
     cacheExchange({
+      resolvers: {
+        Query: {
+          AllPosts: cursorPagination(),
+        },
+      },
       updates: {
         Mutation: {
           logout: (_result, args, cache, info) => {
